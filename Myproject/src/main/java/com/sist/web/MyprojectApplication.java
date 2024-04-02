6695c86c2c5a1b30f9cd594eab8e4afcd5102355
Limhagyeong <aamumu88@gmail.com>
2024-04-03 04:07:23 +0900

    MyThymeleaf

--- Myproject/src/main/java/com/sist/web/MyprojectApplication.java
+++ Myproject/src/main/java/com/sist/web/MyprojectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.sist.web.controller","com.sist.web.dao","com.sist.web.entity"})
 @SpringBootApplication
-
 public class MyprojectApplication {
 
 	public static void main(String[] args) {
